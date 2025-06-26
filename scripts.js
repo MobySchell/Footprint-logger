e4e742d910525ac69f7798e84c307e98eca7e1d5
MobySchell <jagschell2000@gmail.com>
2025-06-26 15:27:02 +0200

    Add Local Storage functionality

---
+++ scripts.js
@@ -0,0 +1,53 @@
+/* 
+Local Storage:
+window.localStorage.setItem();
+window.localStorage.getItem();
+window.localStorage.removeItem();
+window.localStorage.clear();
+window.localStorage.key();
+
+//adding value as a variable
+let fullName = "chime chibuike princewill";
+window.localStorage.setItem("key", fullName);
+
+//adding value as a string
+window.localStorage.setItem("key", "chime chibuike princewill" );
+
+let data = window.localStorage.getItem("key");console.log(data);
+//chime chibuike princewill
+
+// Remove data
+window.localStorage.removeItem('key')
+
+// Clear Storage
+window.localStorage.clear()
+
+//to set an object to the local storage
+const object = {
+    firstName: "chibuike",
+    lastName: "princewill",
+    skills: "software development",
+    email: "princewillchime43@gmail.com",
+    phone: `+234${8169543479}`
+};
+localStorage.setItem("profile", JSON.stringify(object));
+
+// Retrieve object
+let result = JSON.parse(localStorage.getItem("profile"));console.log(result);
+*/
+
+function sendToLocal() {
+    var dropActivities = document.getElementById("activities").value;
+    var inputActivities = document.getElementById("manual-input-field").value;
+    var activitiesBlock = document.getElementById("activities");
+
+    if (dropActivities === "not available" || dropActivities === "") {
+        window.localStorage.setItem("user-input", inputActivities);
+        console.log(window.localStorage);
+        activitiesBlock.innerHTML =
+            '<option value=""></option><option value="driving">Driving</option><option value="meat consumption">Meat Consumption</option><option value="electricity use">Electricity Use</option><option value="not available">Not Available</option>';
+    } else {
+        window.localStorage.setItem("drop-input", dropActivities);
+        console.log(window.localStorage);
+    }
+}
